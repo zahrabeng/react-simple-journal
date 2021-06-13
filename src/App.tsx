e4b31733c95a3137576bdacba7e9d2693c51d971
Richard Ng <richardcrng@gmail.com>
2021-06-13 20:22:47 +0100

    Use components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { greet } from "./utils/greet";
+import MainContent from "./components/MainContent";
+import PageHeader from "./components/PageHeader";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <PageHeader />
+      <MainContent />
+    </>
+  );
 }
 
 export default App;
